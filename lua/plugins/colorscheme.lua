890eccca958c4fe6beaf6aa59e387be4fcd95c55
1mmerse <yimmerse@163.com>
2023-08-30 15:33:33 +0800

    colorscheme onedark to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
+    {
+        "catppuccin/nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000,
     },
-  },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin-mocha",
+        },
+    },
 }
